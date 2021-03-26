739b4027b142e9a3c655cfaefdc300a7aafcfb75
bjoern.brueggemann <Bjoern Brueggemann Bjoern.Brueggemann@extern.dkbcodefactory.com>
2021-03-26 11:57:50 +0100

    Build initial SpringBoot multi module project.

---
+++ application/src/main/kotlin/org/vuejs/tutorial/VuejsTutorialApplication.kt
@@ -0,0 +1,11 @@
+package org.vuejs.tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VuejsTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<VuejsTutorialApplication>(*args)
+}
